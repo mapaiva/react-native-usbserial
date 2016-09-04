9608e47d70535a883f62854f74f07cc6f7209955
Matheus Paiva <matheus.a.paiva@gmail.com>
2016-09-04 14:32:11 -0300

    Initial test module

---
+++ android/src/main/java/com/bmateam/reactnativeusbserial/ReactUSBSerialModule.java
@@ -0,0 +1,22 @@
+package com.bmateam.reactnativeusbserial;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class ReactUSBSerialModule extends ReactContextBaseJavaModule {
+
+    public ReactUSBSerialModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "ReactNativeUSBSerial";
+    }
+
+    @ReactMethod
+    public String olokoBixu() {
+        return "Vindo do Java olha a fera ai";
+    }
+}
